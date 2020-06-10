0b5956efe85a2de6b5d866c1474904ef60b30262
Jc51aT <32559153+Jc51aT@users.noreply.github.com>
2020-06-10 19:30:04 -0400

    gitignore

--- markov_chain.py
+++ markov_chain.py
@@ -3,10 +3,10 @@ import random
 import sys
 import string 
 
-def gen_model(text, order):
+def gen_model(text, order=2):
     model = {}
     
-    for i in range(0, len(text)-2):
+    for i in range(0, len(text)- order):
         fragment = text[i] 
         next_word = text[i+1]
         if fragment not in model:
@@ -24,7 +24,7 @@ def get_next_word(model, fragment):
             wr.append(w)
     return random.choice(wr)
 
-def generate_lyrics(lyrics, order, length):
+def generate_lyrics(lyrics, order=2, length=100):
     model = gen_model(lyrics, order)
     curr_frag = lyrics[random.randint(0,len(lyrics)-1)]
     output= ""
@@ -55,8 +55,8 @@ And pessimists never struck my nerve
 And that's a riff, gonna plead this case
 The reason my power's here on earth
 Salute the truth, when the prophet say"""
-#text = text.translate(str.maketrans('', '', string.punctuation))
-#text = text.split()
+text = text.translate(str.maketrans('', '', string.punctuation))
+text = text.split()
 
 if __name__ == "__main__":
     generate_lyrics(text, int(sys.argv[1]), int(sys.argv[2]))

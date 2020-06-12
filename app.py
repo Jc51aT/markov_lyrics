97ebb35cf062847f0349c7df2bcc4218c1eab7cf
Jc51aT <32559153+Jc51aT@users.noreply.github.com>
2020-06-12 17:31:18 -0400

    reading lyrics

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, url_for, redirect, request
-import lyricsgeinus as genius
+import lyricsgenius as genius
 import json 
 import os 
-import markov_chain
-
-api = genius.Genius(os.environ.get('API_K'))
+import markov_chain as mc
+from os.path import join, dirname
+from dotenv import load_dotenv
+ 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+ 
+# Accessing variables.
+api_key = os.getenv('API_KEY')
+api = genius.Genius(api_key)
+api.verbose = False # Turn off status messages
+api.remove_section_headers = True # Remove section headers (e.g. [Chorus]) from lyrics when searching
+api.skip_non_songs = False # Include hits thought to be non-songs (e.g. track lists)
+api.excluded_terms = ["(Remix)", "(Live)"] # Exclude songs with these words in their title
 
 app = Flask(__name__)
 app.debug = True
@@ -22,24 +33,30 @@ def lyrics():
         return redirect(url_for('index'))
 
     
-    artist = api.search_artist(artist_name, max_songs=20, take_first_result=True)
-    api.save_artists(artist, overwrite=True)
-
-    lyrics = ''
-    for lyric_dict in lyric_list:
-        lyrics += lyric_dict['snippet'].replace('...', '') + ' '
-
-
-    mc = markov_chain()
-    mc.generateDatabase(lyrics)
-
-
-    result = []
-    for line in range(0, lines):
-        result.append(mc.generateString())
+    artist = api.search_artist(artist_name, max_songs=3)
+    artist.save_lyrics('lyrics', overwrite=True)
+    # Opening JSON file 
+    f = open('lyrics.json',)  
+    lyric_list = json.load(f)
     
 
-    return render_template('lyrics.html', result=['hello', 'world'], artist=artist)
+    lyrics = ''
+    songs = lyric_list['songs']
+    print(type(songs[0]))
+    for lyric_dict in songs:
+        lyrics += lyric_dict['lyrics'].replace('...', '') + ' '
+
+
+    
+    result = mc.generate_lyrics(lyrics)
+
+
+    #result = []
+    #for line in range(0, lines):
+    #    result.append(mc.generateString())
+    
+
+    return render_template('lyrics.html', result=result, artist=artist)
 
 if __name__ == '__main__':
     app.run()
--- markov_chain.py
+++ markov_chain.py
@@ -25,6 +25,10 @@ def get_next_word(model, fragment):
     return random.choice(wr)
 
 def generate_lyrics(lyrics, order=2, length=100):
+    text = lyrics
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = text.split()
+    lyrics = text
     model = gen_model(lyrics, order)
     curr_frag = lyrics[random.randint(0,len(lyrics)-1)]
     output= ""
@@ -32,31 +36,8 @@ def generate_lyrics(lyrics, order=2, length=100):
         new_word = get_next_word(model, curr_frag)
         output += new_word + " "
         curr_frag = new_word
-    print(output)
+    return output
 
-
-
-text = """See, my pedigree most definitely don't tolerate the front
-Shit I've been through prolly offend you
-This is Paula's oldest son
-I know murder, conviction
-Burners, boosters, burglars, ballers, dead, redemption
-Scholars, fathers dead with kids
-And I wish I was fed forgiveness
-Yeah, yeah, yeah, yeah, soldier's DNA
-Born inside the beast
-My expertise checked out in second grade
-When I was nine, on cell, motel, we didn't have nowhere to stay
-At twenty-nine, I've done so well, hit cartwheel in my estate
-And I'm gon' shine like I'm supposed to antisocial, extrovert
-And excellent mean the extra work
-And absent-ness what the fuck you heard
-And pessimists never struck my nerve
-And that's a riff, gonna plead this case
-The reason my power's here on earth
-Salute the truth, when the prophet say"""
-text = text.translate(str.maketrans('', '', string.punctuation))
-text = text.split()
 
 if __name__ == "__main__":
     generate_lyrics(text, int(sys.argv[1]), int(sys.argv[2]))
